ce8088a8f608883c5f404c7c3d40047062146cef
James Oliver <james@v-daq.com.au>
2021-11-16 17:48:08 +1100

    use androidx

--- android/library/src/main/java/com/polidea/multiplatformbleadapter/Characteristic.java
+++ android/library/src/main/java/com/polidea/multiplatformbleadapter/Characteristic.java
@@ -2,8 +2,8 @@ package com.polidea.multiplatformbleadapter;
 
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.polidea.multiplatformbleadapter.utils.ByteUtils;
 import com.polidea.multiplatformbleadapter.utils.Constants;
--- android/library/src/main/java/com/polidea/multiplatformbleadapter/Descriptor.java
+++ android/library/src/main/java/com/polidea/multiplatformbleadapter/Descriptor.java
@@ -1,7 +1,7 @@
 package com.polidea.multiplatformbleadapter;
 
 import android.bluetooth.BluetoothGattDescriptor;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import com.polidea.multiplatformbleadapter.utils.ByteUtils;
 import com.polidea.multiplatformbleadapter.utils.IdGenerator;
